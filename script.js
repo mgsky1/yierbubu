f0e6c103bd88d599954f402bfac3c792a5ab6de1
Martin Huang <563044462@qq.com>
2025-05-25 13:26:57 +0800

    pref: text

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ noButton.addEventListener("click", function() {
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!我可是超级爱你的呦!! ( >᎑<)♡︎ᐝ</h1>
+            <h1 class="yes-text">!!!我是很笨很傻很蠢，但是我可是超级爱你的呦!!我会听你的话的 ( >᎑<)♡︎ᐝ</h1>
             <img src="images/hug.gif" alt="拥抱" class="yes-image">
         </div>
     `;
